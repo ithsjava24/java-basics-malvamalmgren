6a16a3387fd966cfb7bb92f94389de9f912bc11b
Malva Malmgren <malva.malmgren@gmail.com>
2024-09-19 17:56:55 +0200

    Sorting update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,11 +11,17 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         String[] hours = new String[24];
+//        for (int i = 0; i < 24; i++) {
+//            String hour1 = String.format("%02d", i);
+//            String hour2 = String.format("%02d", (i + 1) % 24);
+//            hours[i] = hour1 + "-" + hour2;
+//        }
         for (int i = 0; i < 24; i++) {
             String hour1 = String.format("%02d", i);
-            String hour2 = String.format("%02d", (i + 1) % 24);
+            String hour2 = (i == 23) ? "24" : String.format("%02d", (i + 1) % 24); // Fixar 23-24 formatet
             hours[i] = hour1 + "-" + hour2;
         }
+
 
         ArrayList<Integer> pricePerHour = new ArrayList<>(24);
 
@@ -26,11 +32,10 @@ public class App {
             choice = sc.nextLine().toLowerCase();
 
             switch (choice) {
-                case "1":
+                case "1" ->
                     // Inmatning av elpriser för 24 timmar
-                    input(hours, pricePerHour, sc);
-                    break;
-                case "2":
+                        input(hours, pricePerHour, sc);
+                case "2" -> {
                     //När alternativ 2 väljs på menyn så ska programmet skriva ut lägsta priset, högsta priset samt vilka
                     //timmar som detta infaller under dygnet. Dygnets medelpris ska också räknas fram och presenteras
                     //på skärmen. Se testerna för önskat format på utmatningen.
@@ -42,42 +47,36 @@ public class App {
                     System.out.print("Lägsta pris: " + hours[stats.minIndex] + ", " + stats.min() + " öre/kWh\n"
                             + "Högsta pris: " + hours[stats.maxIndex] + ", " + stats.max() + " öre/kWh\n"
                             + "Medelpris: " + String.format("%.2f", stats.average()) + " öre/kWh\n");
-                    break;
-                case "3":
+                }
+                case "3" -> {
                     if (pricePerHour.isEmpty()) {
-                        throw new RuntimeException();
-                        //System.out.println("Listan är tom, inget att sortera.");
-                        //continue;
+                        System.out.println("Listan är tom, inget att sortera.");
+                        continue;
                     }
                     //sortering
-                    ValueAndIndex priceAndHour = new ValueAndIndex(pricePerHour, hours);
-                    sortPrices(priceAndHour);
-//                    for (ValueAndIndex v : priceAndHour) {
-//                        System.out.println("Index: " + v.index() + " Value: " + v.value());
-//                    }
+//                    ValueAndIndex priceAndHour = new ValueAndIndex(pricePerHour, hours);
+//                    sortPrices(priceAndHour);
+                    List<ValueAndIndex> priceAndHourList = new ArrayList<>();
+                    for (int i = 0; i < pricePerHour.size(); i++) {
+                        priceAndHourList.add(new ValueAndIndex(pricePerHour.get(i), hours[i]));
+                    }
+                    sortPrices(priceAndHourList);
 
-                    break;
-                case "4":
-                    System.out.println("");
-                    break;
-                case "5":
-                    System.out.println("");
-                    break;
-                case "e":
-                    System.out.println("");
-                    break;
-                default:
-                    System.out.print("Otillåtet svar, välj ett av alternativen...\n");
-                    break;
+                    // Output sorted prices and hours
+                    for (ValueAndIndex v : priceAndHourList) {
+                        System.out.print("\n" + v.hour() + " " + v.price() + " öre");
+                    }
+                    System.out.print("\n");
+                }
+                    case "4" -> System.out.println("");
+                case "5" -> System.out.println("");
+                case "e" -> System.out.println("");
+                default -> System.out.print("Otillåtet svar, välj ett av alternativen...\n");
             }
-
-            //Sortera!
-
-
-//            int lowestPrice = pricePerHour.get(0);
-//            int highestPrice = pricePerHour.get(1);
-//            int mediumPrice = pricePerHour.get(2);
         }
+
+        //Sortera!
+    }
 
 //        1. Inmatning
 //        Senaste året har elpriserna blivit högre och varierar mycket. Det här programmet ska kunna hjälpa
@@ -93,57 +92,32 @@ public class App {
 //        System.out.println("Hello There!");
 
 
-    }
-    record ValueAndIndex(ArrayList<Integer> value, String[] index) {}
+    //class ValueAndIndex(ArrayList<Integer> value, String[] index) {}
+    static class ValueAndIndex implements Comparable<ValueAndIndex> {
+        private int price;
+        private String hour;
 
-    private static void sortPrices(ValueAndIndex priceAndHour) {
-        //3. ValueAndIndex priceAndHour = new ValueAndIndex(pricePerHour, hours);
-        //vill ha priserna och timmarna, kopior
-        ArrayList<Integer> pricePerHour = priceAndHour.value;
-        String[] hours = new String[priceAndHour.index.length];
-        System.out.println("HI 1");
-
-        //temps
-//        ArrayList<Integer> sortedPrices = new ArrayList<>(pricePerHour);
-//        String[] sortedHours = new String[pricePerHour.size()];
-
-        //sortera ören
-        for (int i = 0; i < pricePerHour.size(); i++) {
-            int max = pricePerHour.get(i);
-            for (int j = i + 1; j < pricePerHour.size(); j++) {
-                if (pricePerHour.get(j) > max) {
-                    max = j;
-                }
-            }
-            int tempPrice = pricePerHour.get(i);
-            pricePerHour.set(i, pricePerHour.get(max));
-            pricePerHour.set(max, tempPrice);
-
-            String tempHour = hours[i];
-            hours[i] = hours[max];
-            hours[max] = tempHour;
-
+        public ValueAndIndex(int price, String hour) {
+            this.price = price;
+            this.hour = hour;
         }
 
+        public int price() {
+            return price;
+        }
 
-//        for (int i = 0; i < pricePerHour.size(); i++) { //Går genom priserna
-//            int maxIndex = i;
-//            for (int j = i + 1; j < hours.length; j++) {
-//                if (pricePerHour.get(j) > pricePerHour.get(maxIndex)) {
-//
-//                }
-//            }
-//        }
-        System.out.println("HI 2");
+        public String hour() {
+            return hour;
+        }
 
-        System.out.println("HI 3");
-        //ha index för vilken timma
+        @Override
+        public int compareTo(ValueAndIndex other) {
+            return Integer.compare(other.price, this.price); // Sort by price in descending order
+        }
+    }
 
-        //PRINT
-//        for (ValueAndIndex v : priceAndHour) {
-//            System.out.println("Index: " + v.index() + " Value: " + v.value());
-//        }
-//        System.out.print();
+    private static void sortPrices(List<ValueAndIndex> priceAndHourList) {
+        Collections.sort(priceAndHourList); // Sort based on price, using the Comparable interface
     }
 
     private static void printMenu() {
